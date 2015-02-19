9eeeca79eaa04d01fb096cf5f90d7c0acd5304d2
Javier Pérez <javier@weareknitters.com>
2015-02-19 10:40:33 +0100

    cleaning

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 
 require 'adonise/setup.php';
-
-/*
-// define('APP_SCRIPT_FILE',basename(__FILE__));
-define('APP_SCRIPT_FILE',''); // modrewrite
-
-require 'setup.php';
-
-require 'run.php';
-*/
